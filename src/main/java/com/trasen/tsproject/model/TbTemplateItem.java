9586174699c8b94dbe17c61f5e21ede7cf5a91bd
zhangxiahui <281055769@qq.com>
2017-09-27 10:41:38 +0800

    #项目管理微服务 查询单个模板

--- src/main/java/com/trasen/tsproject/model/TbTemplateItem.java
+++ src/main/java/com/trasen/tsproject/model/TbTemplateItem.java
@@ -1,14 +1,14 @@
 package com.trasen.tsproject.model;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Table(name = "tb_template_item")
+@Getter
+@Setter
 public class TbTemplateItem {
     /**
      * 主键
      */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pkid;
 
     /**
@@ -20,6 +20,10 @@ public class TbTemplateItem {
      * 元素类型（handover：交接单）
      */
     private String type;
+
+    private String code;
+
+    private Integer isRequired;
 
     /**
      * 元素展示格式（text|radio|textarea）
@@ -39,7 +43,6 @@ public class TbTemplateItem {
     /**
      * 是否有效
      */
-    @Column(name = "is_vaild")
     private Integer isVaild;
 
     /**
@@ -52,165 +55,5 @@ public class TbTemplateItem {
      */
     private Integer px;
 
-    /**
-     * 获取主键
-     *
-     * @return pkid - 主键
-     */
-    public Integer getPkid() {
-        return pkid;
-    }
 
-    /**
-     * 设置主键
-     *
-     * @param pkid 主键
-     */
-    public void setPkid(Integer pkid) {
-        this.pkid = pkid;
-    }
-
-    /**
-     * 获取模板元素名
-     *
-     * @return name - 模板元素名
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * 设置模板元素名
-     *
-     * @param name 模板元素名
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * 获取元素类型（handover：交接单）
-     *
-     * @return type - 元素类型（handover：交接单）
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * 设置元素类型（handover：交接单）
-     *
-     * @param type 元素类型（handover：交接单）
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    /**
-     * 获取元素展示格式（text|radio|textarea）
-     *
-     * @return input - 元素展示格式（text|radio|textarea）
-     */
-    public String getInput() {
-        return input;
-    }
-
-    /**
-     * 设置元素展示格式（text|radio|textarea）
-     *
-     * @param input 元素展示格式（text|radio|textarea）
-     */
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    /**
-     * 获取表单文本框长度（页面渲染样式用）
-     *
-     * @return length - 表单文本框长度（页面渲染样式用）
-     */
-    public Integer getLength() {
-        return length;
-    }
-
-    /**
-     * 设置表单文本框长度（页面渲染样式用）
-     *
-     * @param length 表单文本框长度（页面渲染样式用）
-     */
-    public void setLength(Integer length) {
-        this.length = length;
-    }
-
-    /**
-     * 获取模块
-     *
-     * @return module - 模块
-     */
-    public String getModule() {
-        return module;
-    }
-
-    /**
-     * 设置模块
-     *
-     * @param module 模块
-     */
-    public void setModule(String module) {
-        this.module = module;
-    }
-
-    /**
-     * 获取是否有效
-     *
-     * @return is_vaild - 是否有效
-     */
-    public Integer getIsVaild() {
-        return isVaild;
-    }
-
-    /**
-     * 设置是否有效
-     *
-     * @param isVaild 是否有效
-     */
-    public void setIsVaild(Integer isVaild) {
-        this.isVaild = isVaild;
-    }
-
-    /**
-     * 获取级别，生成打印表格时使用
-     *
-     * @return level - 级别，生成打印表格时使用
-     */
-    public Integer getLevel() {
-        return level;
-    }
-
-    /**
-     * 设置级别，生成打印表格时使用
-     *
-     * @param level 级别，生成打印表格时使用
-     */
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    /**
-     * 获取排序
-     *
-     * @return px - 排序
-     */
-    public Integer getPx() {
-        return px;
-    }
-
-    /**
-     * 设置排序
-     *
-     * @param px 排序
-     */
-    public void setPx(Integer px) {
-        this.px = px;
-    }
 }
