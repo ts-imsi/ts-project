062db00e5d10bb75f92e274dbcf5a55d83bb7b31
zhangxiahui <281055769@qq.com>
2017-11-02 16:08:00 +0800

    #项目管理微服务 修改项目初始计划时间

--- src/main/java/com/trasen/tsproject/service/ProjectPlanService.java
+++ src/main/java/com/trasen/tsproject/service/ProjectPlanService.java
@@ -100,6 +100,7 @@ public class ProjectPlanService {
                 upBoo = true;
             }
             if(upBoo){
+                plan.setOperator(VisitInfoHolder.getShowName());
                 tbProjectPlanLogMapper.updatePlanTime(plan);
                 boo = true;
             }
